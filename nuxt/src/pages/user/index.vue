46770710c2ebf03eb1de59a912cb04a3a2f23985
MusashiOkuoka <musashi.okuoka@i-standard.biz>
2022-08-29 20:51:47 +0900

    一旦コミット

--- nuxt/src/pages/user/index.vue
+++ nuxt/src/pages/user/index.vue
@@ -1,29 +1,49 @@
 <script setup>
-    const { data: users } = await useFetch('http://localhost:3000/api/user');
+import { ref } from "@vue/reactivity";
+
+const { data: users } = await useFetch("http://localhost:3000/api/user");
+const firstName = ref("");
+const lastName = ref("");
+const createUser = () => {
+  const data = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+  };
+
+  try {
+    const {data: user} = useFetch("/api/user", {
+        method: "POST",
+        body: data,
+    });
+    console.log(user);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
 </script>
 
 <template>
-    <div>
-        <form @submit.prevent="submit">
-            <!-- <input v-model="firstName" placeholder="firstName" />
-            <input v-model="lastName" placeholder="lastName" /> -->
-            <button type="submit">送信</button>
-        </form>
+  <div>
+    <form @submit.prevent="createUser">
+      <input v-model="firstName" placeholder="firstName" />
+      <input v-model="lastName" placeholder="lastName" />
+      <button type="submit">送信</button>
+    </form>
 
-
-        <table>
-            <thead>
-                <tr>
-                    <td>firstName</td>
-                    <td>lastName</td>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(user, index) in users" :key="index">
-                    <td>{{ user.firstName }}</td>
-                    <td>{{ user.lastName }}</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <td>firstName</td>
+          <td>lastName</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(user, index) in users" :key="index">
+          <td>{{ user.firstName }}</td>
+          <td>{{ user.lastName }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
