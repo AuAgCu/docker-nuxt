2612f852748733008a7b933126f07c14fa666b76
MusashiOkuoka <musashi.okuoka@i-standard.biz>
2022-09-02 11:25:44 +0900

    一旦コミット

--- nuxt/src/nuxt.config.ts
+++ nuxt/src/nuxt.config.ts
@@ -2,5 +2,14 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: ['request']
+    modules: ['nuxt-vite'],
+    vite: {
+        server: {
+            proxy: {
+                "/api/": {
+                    target: "http://go:3001/",
+                }
+            },
+        }
+    },
 });
--- nuxt/src/server/api/[...].ts
+++ nuxt/src/server/api/[...].ts
@@ -1,14 +1,8 @@
 import type { IncomingMessage, ServerResponse } from "http";
-import request from 'request';
 
 
 export default async (req: IncomingMessage, res: ServerResponse) => {
     const proxyRequestHeaders = Object.assign({}, req.headers);
 
     const proxyUrl = 'http://go:3001/';
-    request({
-        url: proxyUrl,
-        method: req.method,
-        headers: proxyRequestHeaders,
-    }).pipe(res);
 };
